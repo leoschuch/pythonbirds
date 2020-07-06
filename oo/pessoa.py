0385307ad9e369a59a2e92cbb0089be157bdea02
leoschuch <leo.schuch@gmail.com>
2020-07-06 14:03:37 -0300

    Decorator

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2 # atributo default ou de classe, que não irá consumir memória e ele serve para toda a classe
+
     def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
@@ -7,7 +9,20 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    # método decorator abaixo:
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
+
+    luciano = Pessoa(nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+
     renzo = Pessoa(nome='Renzo')
     luciano = Pessoa(renzo, nome='Luciano')
     print(luciano.filhos)
@@ -21,3 +36,8 @@ if __name__ == '__main__':
 
     # para visualizar tudo de uma classe:
     print(luciano.__dict__)
+
+    # exemplo do método estático @metodo_estático
+    print(Pessoa.metodo_estatico())
+    # outro exemplo @
+    print(Pessoa.nome_e_atributos_de_classe())
