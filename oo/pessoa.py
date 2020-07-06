609c9b7b73f4f3dd280dcbb947823229b1e10d09
leoschuch <leo.schuch@gmail.com>
2020-07-06 13:42:08 -0300

    Criado e removido atributo dinâmico de objeto do tipo Pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -11,3 +11,13 @@ if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo')
     luciano = Pessoa(renzo, nome='Luciano')
     print(luciano.filhos)
+
+    # é possível criar um atributo em tempo real de execução:
+    luciano.sobrenome = 'Ramalho'
+    print(luciano.sobrenome)
+
+    # é possível remover um atributo em tempo real de execução:
+    del luciano.filhos
+
+    # para visualizar tudo de uma classe:
+    print(luciano.__dict__)
