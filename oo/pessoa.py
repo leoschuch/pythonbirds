b7f73214c62867b26786b367deb378cc7e9b3921
leoschuch <leo.schuch@gmail.com>
2020-07-06 13:33:39 -0300

    Criados atributos complexo: Filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,13 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Leonardo')
-    print(p.nome)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(luciano.filhos)
